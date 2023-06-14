1f34cf36572ac2499f218263b9d1c093d00a6801
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2023-06-14 21:50:20 +0530

    image_processing

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import streamlit as st
-
+import cv2
+import numpy as np
+import pandas as pd
 st.write("hello world")
 # cameera input
-data_1 = st.camera_input(label = "hello world")
-location = st.session_state.location
-if location is not None:
-    st.write(location)
-#st.write(data_1)
+data_image = st.camera_input(label = "hello world")
+
+
+if data_image is not None:
+    bytes_data = data_image.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    cv2_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
+    st.image(cv2_img)
