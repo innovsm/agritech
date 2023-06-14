ead162030b051c3a099dab375d1caf835f32ff6a
SHUBHANSHU MISHRA <rollsroyace360@gmail.com>
2023-06-14 21:42:36 +0530

    location

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import streamlit as st
 st.write("hello world")
 # cameera input
 data_1 = st.camera_input(label = "hello world")
+location = st.session_state.location
+if location is not None:
+    st.write(location)
 #st.write(data_1)
